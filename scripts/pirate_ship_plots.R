8c5d0a98703a4eb1e691fa2728518b882b464010
Elizabeth Brooks <merimaat1@gmail.com>
2022-07-06 00:41:29 -0400

    Adding initial playground directories and files

---
+++ scripts/pirate_ship_plots.R
@@ -0,0 +1,80 @@
+#### Colorful R Plots with Wes Anderson Palettes & ggplot2 - Pirate Ship Features
+# import the pirate ship data from the PiratesShip.csv file
+ships <- read.csv("PiratesShip.csv")
+
+# view the column names of the ship data
+colnames(ships)
+
+# view the first few lines of ship data
+head(ships)
+
+# explore the relationship between Sails and CrewCapacity
+# first, create a scatter plot
+ggplot(data = ships, aes(x = Sails, y = CrewCapacity)) +
+  geom_point()
+
+# color the scatter plot by MaidenYear
+ggplot(data = ships, aes(x = Sails, y = CrewCapacity, color = MaidenYear)) +
+  geom_point()
+
+# create a colorful plot by searching the internet for "ggplot wes anderson"
+# https://github.com/karthik/wesanderson
+# https://rforpoliticalscience.com/2020/07/26/make-wes-anderson-themed-graphs-with-wesanderson-package-in-r/
+
+# if not already, install the wesanderson package
+#install.packages("wesanderson")
+
+# load the wesanderson package
+library(wesanderson)
+
+# checkout the color palette options
+names(wes_palettes)
+
+# look at the info for the wes_palette function
+?wes_palette
+
+# look at the info for the scale_color_gradientn function
+?scale_color_gradientn
+
+# color the scatter plot using wes_palettes and scale_color_gradientn
+ggplot(data = ships, aes(x = Sails, y = CrewCapacity, color = MaidenYear)) +
+  geom_point() +
+  scale_color_gradientn(colors = wes_palette("Zissou1", type = "continuous"))
+
+# add axis and plot titles
+ggplot(data = ships, aes(x = Sails, y = CrewCapacity, color = MaidenYear)) +
+  geom_point() +
+  scale_color_gradientn(colors = wes_palette("Zissou1", type = "continuous")) +
+  labs(title="Pirate Ship Crew Capacity by Sails", 
+       x ="Number of Sails", 
+       y = "Crew Capacity")
+
+# adjust the axis and plot title colors
+ggplot(data = ships, aes(x = Sails, y = CrewCapacity, color = MaidenYear)) +
+  geom_point() +
+  scale_color_gradientn(colors = wes_palette("Zissou1", type = "continuous")) +
+  labs(title="Pirate Ship Crew Capacity by Sails", 
+       x ="Number of Sails", 
+       y = "Crew Capacity") +
+  theme(
+    plot.title = element_text(color="darkgreen", size=14, face="bold.italic"),
+    axis.title.x = element_text(color="darkblue", size=14, face="bold"),
+    axis.title.y = element_text(color="darkred", size=14, face="bold")
+  )
+
+# center the plot title
+ggplot(data = ships, aes(x = Sails, y = CrewCapacity, color = MaidenYear)) +
+  geom_point() +
+  scale_color_gradientn(colors = wes_palette("Zissou1", type = "continuous")) +
+  labs(title="Pirate Ship Crew Capacity by Sails", 
+       x ="Number of Sails", 
+       y = "Crew Capacity") +
+  theme(
+    plot.title = element_text(color="darkgreen", size=14, face="bold.italic"),
+    axis.title.x = element_text(color="darkblue", size=14, face="bold"),
+    axis.title.y = element_text(color="darkred", size=14, face="bold")
+  ) +
+  theme(plot.title = element_text(hjust = 0.5))
+
+# save the last plot using the ggsave function
+ggsave("ship_plot_crew_sails_year_centerTitle.png", plot = last_plot())
---
+++ scripts/pirates_plots.R
@@ -0,0 +1,139 @@
+#### Fun R Plots with ggplot2 - Pirate Features
+# set the working directory
+#setwd("/Users/bamflappy/Documents/rPlayground")
+
+# if not already, install the ggplot2 package
+#install.packages("ggplot2")
+
+# load the ggplot2 library
+library(ggplot2)
+
+# import the pirate data from the PiratesPirate.csv file
+pirates <- read.csv("PiratesPirate.csv")
+
+# view the column names of the pirates data
+colnames(pirates)
+
+# view the first few lines of pirates data
+head(pirates)
+
+# check out the basic ggplot function and geoms
+# https://ggplot2.tidyverse.org/reference/
+# https://datacarpentry.org/r-socialsci/04-ggplot2/index.html
+
+### One Dimension - Teeth or Limbs
+
+# start with plotting only the Limbs dimension of the pirates data
+ggplot(data = pirates, aes(x = Limbs)) +
+  geom_bar()
+
+# next, plot only the Teeth dimension of the pirates data
+ggplot(data = pirates, aes(x = Teeth)) +
+  geom_bar()
+
+### Two Dimensions - Teeth & Limbs
+
+# create a scatter plot comparing the two dimensions of Limbs vs Teeth
+ggplot(data = pirates, aes(x = Limbs, y = Teeth)) +
+  geom_point()
+
+# modify the scatter plot to include the count of pirates at each point
+ggplot(data = pirates, aes(x = Limbs, y = Teeth)) +
+  geom_count()
+
+# check the data for the outlier pirate with 2.5 Limbs
+pirates[pirates$Limbs == "2.5", ]
+
+# change to a boxplot to better describe the data at each point
+ggplot(data = pirates, aes(x = Limbs, y = Teeth)) +
+  geom_boxplot()
+
+# check the info for the factor function
+?factor
+
+# use the factor function to make the different numbers of Limbs into categories
+ggplot(data = pirates, aes(x = factor(Limbs), y = Teeth)) +
+  geom_boxplot()
+
+# create a more detailed plot using colors to add another dimension
+# look up color options on the internet by searching "ggplot boxplot color"
+# example 1 from r-graph-gallery.com/264-control-ggplot2-boxplot-colors.html
+ggplot(data = pirates, aes(x = factor(Limbs), y = Teeth)) +
+  geom_boxplot(color="red", fill="orange", alpha=0.2)
+
+# example 2 from r-graph-gallery.com/264-control-ggplot2-boxplot-colors.html
+ggplot(data = pirates, aes(x = factor(Limbs), y = Teeth, fill = Origin)) +
+  geom_boxplot(alpha=0.3) +
+  theme(legend.position="none")
+
+### Three Dimensions - Teeth & Limbs & Origin
+
+# create a set of boxplots with one for each Origin of pirates
+# look up facet options on the internet by searching "ggplot boxplot facet"
+# www.sthda.com/english/wiki/ggplot2-facet-split-a-plot-into-a-matrix-of-panels
+ggplot(data = pirates, aes(x = factor(Limbs), y = Teeth)) +
+  geom_boxplot(color="darkgreen", fill="orange", alpha=0.2) +
+  facet_wrap(~ Origin)
+
+# change the appearance of the individual plot titles
+# www.sthda.com/english/wiki/ggplot2-facet-split-a-plot-into-a-matrix-of-panels
+ggplot(data = pirates, aes(x = factor(Limbs), y = Teeth)) +
+  geom_boxplot(color="darkgreen", fill="orange", alpha=0.2) +
+  facet_wrap(~ Origin) +
+  theme(strip.background = element_rect(colour="black", fill="white", 
+                                        size=1.5, linetype="solid"))
+
+# change the axis titles and add a plot title
+# look up how to add titles on the internet by searching "ggplot title"
+# top result www.sthda.com/english/wiki/ggplot2-title-main-axis-and-legend-titles
+ggplot(data = pirates, aes(x = factor(Limbs), y = Teeth)) +
+  geom_boxplot(color="darkgreen", fill="orange", alpha=0.2) +
+  facet_wrap(~ Origin) +
+  theme(strip.background = element_rect(colour="black", fill="white", 
+                                        size=1.5, linetype="solid")) +
+  labs(title="Comparison of Pirate Teeth and Limb Numbers by Origin", 
+       x ="Number of Limbs", 
+       y = "Number of Teeth")
+
+# adjust the colors of the axis and plot titles
+# www.sthda.com/english/wiki/ggplot2-title-main-axis-and-legend-titles
+ggplot(data = pirates, aes(x = factor(Limbs), y = Teeth)) +
+  geom_boxplot(color="darkgreen", fill="orange", alpha=0.2) +
+  facet_wrap(~ Origin) +
+  theme(strip.background = element_rect(colour="black", fill="white", 
+                                        size=1.5, linetype="solid")) +
+  labs(title="Comparison of Pirate Teeth and Limb Numbers by Origin", 
+       x ="Number of Limbs", 
+       y = "Number of Teeth") +
+  theme(
+    plot.title = element_text(color="red", size=14, face="bold.italic"),
+    axis.title.x = element_text(color="blue", size=14, face="bold"),
+    axis.title.y = element_text(color="purple", size=14, face="bold")
+  )
+
+# center the plot title
+# look up how to center the title on the internet by searching "ggplot center title"
+# https://stackoverflow.com/questions/40675778/center-plot-title-in-ggplot2
+ggplot(data = pirates, aes(x = factor(Limbs), y = Teeth)) +
+  geom_boxplot(color="darkgreen", fill="orange", alpha=0.2) +
+  facet_wrap(~ Origin) +
+  theme(strip.background = element_rect(colour="black", fill="white", 
+                                        size=1.5, linetype="solid")) +
+  labs(title="Comparison of Pirate Teeth and Limb Numbers by Origin", 
+       x ="Number of Limbs", 
+       y = "Number of Teeth") +
+  theme(
+    plot.title = element_text(color="red", size=14, face="bold.italic"),
+    axis.title.x = element_text(color="blue", size=14, face="bold"),
+    axis.title.y = element_text(color="purple", size=14, face="bold")
+  ) +
+  theme(plot.title = element_text(hjust = 0.5))
+
+# check out the info for the ggsave function
+?ggsave
+
+# check out the info for the last_plot function
+?last_plot
+
+# save the last plot using the ggsave function
+ggsave("pirates_plot_teeth_limbs_origin.png", plot = last_plot())
